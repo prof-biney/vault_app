3602f4d767a0740be6cc1511fc3d62f5da415536
abiney1321 <abiney1321@gmail.com>
2025-07-15 12:06:41 +0000

    setup theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,70 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+// tailwind.config.js
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fill: {
+          1: "rgba(255, 255, 255, 0.10)",
+        },
+        bankGradient: "#0179FE",
+        indigo: {
+          500: "#6172F3",
+          700: "#3538CD",
+        },
+        success: {
+          25: "#F6FEF9",
+          50: "#ECFDF3",
+          100: "#D1FADF",
+          600: "#039855",
+          700: "#027A48",
+          900: "#054F31",
+        },
+        pink: {
+          25: "#FEF6FB",
+          100: "#FCE7F6",
+          500: "#EE46BC",
+          600: "#DD2590",
+          700: "#C11574",
+          900: "#851651",
+        },
+        blue: {
+          25: "#F5FAFF",
+          100: "#D1E9FF",
+          500: "#2E90FA",
+          600: "#1570EF",
+          700: "#175CD3",
+          900: "#194185",
+        },
+        sky: {
+          1: "#F3F9FF",
+        },
+        black: {
+          1: "#00214F",
+          2: "#344054",
+        },
+        gray: {
+          25: "#FCFCFD",
+          200: "#EAECF0",
+          300: "#D0D5DD",
+          500: "#667085",
+          600: "#475467",
+          700: "#344054",
+          900: "#101828",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", ...fontFamily.sans],
+        "inter-bold": ["Inter-Bold", ...fontFamily.sans],
+        "inter-semibold": ["Inter-SemiBold", ...fontFamily.sans],
+        "inter-medium": ["Inter-Medium", ...fontFamily.sans],
+        "inter-light": ["Inter-Light", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
