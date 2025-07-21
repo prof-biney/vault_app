eb719d9d11969ca846e750804bd2b525337c58e2
abiney1321 <abiney1321@gmail.com>
2025-07-21 09:01:33 +0000

    run tests

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
-import { Text, View } from "react-native";
+import { images } from "@/constants";
+import { Image, Text, View } from "react-native";
 import "./global.css";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center text-blue-600">
-      <Text className="text-3xl text-blue-600">
-        Edit app/index.tsx to edit this screen.
-      </Text>
+    <View className="flex-1 items-center justify-center ">
+      <Image source={images.logo} className="size-40" resizeMode="contain" />
+
+      <Text className="font-bold text-3xl text-blue-600">Welcome to Vault</Text>
     </View>
   );
 }
--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,11 @@
 import home from "@/assets/icons/home.png";
 
+import logo from "@/assets/images/logo.png";
+
 export const icons = {
   home,
+};
+
+export const images = {
+  logo,
 };
